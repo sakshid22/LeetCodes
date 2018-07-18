c5434e0096128d8261646b66df40e8d9d647197c
Sakshi Dubey <dubeysakshi22@gmail.com>
2018-07-18 18:11:47 -0400

    Matrix

---
+++ #240 - Search a 2D Matrix II.cpp
@@ -0,0 +1,32 @@
+//240
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        
+    
+
+
+
+//bool searchmatrix( vector<vectot<<int>> & matrix, int target){
+    int m = matrix.size();
+         if(m==0) return false;
+    int n= matrix[0].size();
+   
+    int i=0, j=n-1;
+        while(i<m && j>=0)
+        {
+        if(matrix[i][j] == target)
+            {
+            return true;
+            }
+        else if (matrix[i][j]>target)
+            {
+            j--;
+            }
+        else
+           i++; 
+        }
+    return false;
+}
+    };
