b62eaeecad5b17f67d181f6cd31ac4c4ae936e30
Sakshi Dubey <dubeysakshi22@gmail.com>
2018-07-29 21:12:28 -0400

    Add files via upload

---
+++ #125 - Valid Palindrome.cpp
@@ -0,0 +1,16 @@
+//125
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        
+        int l=0, r=s.length()-1;
+        while(l<r)
+        {
+            while(!isalnum(s[l])  && l<r)   l++;
+            while(!isalnum(s[r])  && l<r)   r--;
+            if(toupper(s[l++]) != toupper(s[r--]))
+                return false;
+        }
+        return true;
+    }
+};
