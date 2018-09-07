9c5497474fde665810ab2d20569c11925c1cdcdd
Sakshi Dubey <dubeysakshi22@gmail.com>
2018-09-07 17:58:51 -0400

    System Design

---
+++ #346 - Moving Average from Data Stream.cpp
@@ -0,0 +1,26 @@
+//346
+
+class MovingAverage {
+public:
+	Moving Average(int size)
+	{
+	this->size = size;
+	sum =0;
+	}
+	double next (int val)
+	{
+	if(q.size() >= size)
+		{
+		sum -= q.front();
+		q.pop();
+		}
+		q.push(val);
+		sum+=val;
+		return sum/q.size();
+	}
+	
+	private:
+		queue <int> q;
+		int size;
+		double sum;
+};
