892ea72b223f0d80d8e64562c531be2e502cdb6e
Sakshi Dubey <dubeysakshi22@gmail.com>
2018-09-07 17:16:38 -0400

    System Design

---
+++ #170 - Two Sum III Data Structure Design.cpp
@@ -0,0 +1,23 @@
+//170
+
+class TwoSum
+{
+public:
+	void add(int number)
+	{
+	++m[numbrer];
+	}
+	bool find(int value)
+	{
+	for(auto a:m)
+		{
+		int t=value-a.first;
+			if((t!=a.first && m.count(t)) || (t==a.first && a.second>1))
+				return true;
+		}
+		return false;
+	
+	}
+private:
+	unordered_map<int,int>m;
+}
