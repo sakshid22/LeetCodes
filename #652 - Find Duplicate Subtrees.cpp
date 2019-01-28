9a027bfabef1b7757ac932c4889a64592b3ea414
Sakshi Dubey <dubeysakshi22@gmail.com>
2019-01-28 16:15:57 -0500

    Tree

---
+++ #652 - Find Duplicate Subtrees.cpp
@@ -0,0 +1,28 @@
+//652
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
+        unordered_map <string, int> m;
+        vector <TreeNode* > res;
+        DFS(root, m, res);
+        return res;
+        
+    }
+    string DFS( TreeNode *root, unordered_map <string,int> &m, vector<TreeNode* >&res)
+    {
+        if (!root) return "";
+        string s = to_string(root->val) + "," + DFS(root->left,m, res) + "," + DFS(root-> right, m, res);
+        if(m[s]++ ==1) res.push_back(root);
+        return s;
+    }
+};
